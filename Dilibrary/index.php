ab30c7521171abb2b0d94c2505d3e6c923221c59
reyesyam <reyes@citruss.mx>
2014-03-31 14:24:20 -0600

    proyecto-index-css

--- Dilibrary/index.php
+++ Dilibrary/index.php
@@ -10,6 +10,31 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        Bievenidos
+        <div id="wrapper">
+            <header>
+                <h1>Bienvenidos </h1>        
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Libros</a></li>
+                    <li><a href="#">Panel</a></li>
+                    <li><a href="#">Foro</a></li>
+                </ul>
+            </nav>
+            <article>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra nisi placerat, sagittis neque eu, pharetra libero. Nulla facilisi. Donec bibendum eros at sodales rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in quam vel tellus tincidunt vehicula. Mauris a dictum nisl, sed auctor felis. Ut pellentesque sodales egestas. Morbi posuere mauris eu arcu ultrices, in volutpat erat lacinia. Nulla a erat in turpis suscipit pellentesque. Integer tristique dapibus suscipit. Nam ultrices id magna a blandit.
+
+Nulla facilisi. Cras volutpat pellentesque dignissim. Vestibulum sed luctus quam. Sed vitae nibh auctor, aliquam urna id, pellentesque augue. Phasellus ut magna posuere, condimentum tellus ac, lacinia tellus. Fusce mollis elementum odio, sit amet consectetur urna tincidunt vel. Cras vitae neque elit. Quisque enim nulla, rutrum in faucibus fermentum, porta vel libero. Quisque nec sem nec nibh vulputate eleifend. Phasellus interdum quis leo suscipit convallis. Curabitur consequat justo consequat, placerat nisi vel, consequat tellus. Aliquam condimentum porta orci, a bibendum elit fringilla eu. Ut id nibh id augue mattis elementum in quis leo. Ut eu luctus purus.
+
+Nunc laoreet accumsan varius. Donec vitae nulla eu neque aliquam rhoncus a sit amet libero. Nullam faucibus sem eleifend nunc rhoncus ullamcorper. Duis laoreet interdum risus id vehicula. Phasellus faucibus consequat nulla, sollicitudin porttitor diam mollis at. Mauris accumsan, ante vitae vulputate lobortis, elit purus placerat metus, a aliquam dolor nibh non nibh. Duis a odio ac lacus vestibulum tristique euismod in dui. Cras magna tellus, sollicitudin quis tellus quis, varius dapibus metus.
+                </p>
+            </article>
+            <footer>
+                Libreri Digital
+            </footer>
+        </div>
+        
     </body>
 </html>
